408c404385be1683ee79e033cf5a79f6d89081aa
Smail <smailbestybay@gmail.com>
2022-06-07 23:45:07 -0700

    pre venv setup

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from . import util
 
@@ -11,7 +12,13 @@ def index(request):
 def entry(request, TITLE):
     if util.get_entry(TITLE) is None:
         return render(request, "encyclopedia/error.html")
-        
+
     return render(request, "encyclopedia/entry.html", {
         "entry": util.get_entry(TITLE)
         })
+
+def search(request, QUERY):
+
+    print(QUERY)
+    if QUERY[2:] in util.list_entries:
+        return redirect("entry", QUERY[2:])
