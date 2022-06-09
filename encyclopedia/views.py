ce940e851a95a16f6be377897a75f25f74eca295
Smail <smailbestybay@gmail.com>
2022-06-08 21:44:48 -0700

    random page complete, markdown complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,13 @@
+from random import random
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from . import util
-import encyclopedia
+from markdown2 import Markdown
 
+from . import util
+# import encyclopedia
+import random
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -15,11 +18,14 @@ def entry(request, TITLE):
     if util.get_entry(TITLE) is None:
         return render(request, "encyclopedia/error.html")
 
+    # need to use safe filter to escape string quotes in the template
     return render(request, "encyclopedia/entry.html", {
-        "entry": util.get_entry(TITLE)
+        "entry": Markdown().convert(util.get_entry(TITLE))
         })
 
 def search(request):
+    # must have get method stated in html
+    # then use request object to gain access to the query
     if 'q' in request.GET:
         if util.get_entry(request.GET['q']) is not None:
             return redirect("entry", request.GET['q']) 
@@ -33,6 +39,10 @@ def search(request):
         "results" : results,
         "q" : request.GET['q']
     })
+
+def randompage(requst):
+    random_int = random.randint(0, len(util.list_entries())-1)
+    return redirect("entry", util.list_entries()[random_int])
 
     
 
