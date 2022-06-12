537f2750666f0538debd25e9fea189565c170e17
Smail <smailbestybay@gmail.com>
2022-06-12 01:38:38 -0700

    make migrations on listing model

--- auctions/models.py
+++ auctions/models.py
@@ -1,39 +1,47 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
 
-class Listing():
+class Listing(models.Model):
     # Listing needs to have:
-    # id
+    # id is set automatically
     # title not null
+    title = models.CharField(max_length=64)
     # description not null
+    description  = models.CharField(max_length=64)
     # starting bid number not null
+    starting_bid = models.IntegerField()
     # URL for an image
+    imgage = models.URLField(blank=True)
     # category of listing (eg. Toys, Fashion, Electronics)
-    # creator user key
+    category = models.CharField(max_length=64, blank=True)
     # active boolean not null
-    pass
+    active = models.BooleanField()
+    # creator user key
 
-class Bid():
-    # id
-    # user key
-    # listing key
-    # bit ammount
-    pass
 
-class Comment():
-    # id
-    # listing key
-    # user key
-    # time
-    # content
-    pass
+    def __str__(self):
+        return f"{self.id}: {self.title}"
 
-class Watchlist():
-    # id
-    # user key
-    # listing key
-    pass
+# class Bid(models.Model):
+#     # id
+#     # user key
+#     # listing key
+#     # bit ammount
+#     pass
+
+# class Comment(models.Model):
+#     # id
+#     # listing key
+#     # user key
+#     # time
+#     # content
+#     pass
+
+# class Watchlist(models.Model):
+#     # id
+#     # user key
+#     # listing key
+#     pass
