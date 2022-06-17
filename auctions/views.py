a781a0a4356e521323ddcc4c7932993166856ab1
Smail <smailbestybay@gmail.com>
2022-06-17 00:12:52 -0700

    create listing backend complete

--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from .models import User, Listing
 from .formhelper import NewListingForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # active listings should display
@@ -65,17 +66,45 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
-# @login_required
+@login_required(login_url='login')
 def create_listing(request):
-    # form to create a new listing must have fields:
-    # title (must be filled)
-    # description (must be filled)
-    # starting bid (must be filled)
-    # URL for an image
-    # category
-    # submit takes to newly created listing
+    if request.method == "POST":
+        # https://docs.djangoproject.com/en/4.0/topics/forms/
+        # instantiate bound form 
+        form = NewListingForm(request.POST)
+        if form.is_valid():
+            
+            # instantiate every field
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            starting_bid = form.cleaned_data["starting_bid"]
+            image = form.cleaned_data["image"]
+            category = form.cleaned_data["category"]
+            # get id of the user creating the listing.
+            # all model instances have id so need user.id
+            # but in our case we need an instance of a user as a whole.
+            owner = request.user
+
+            # instantiate an object of models class
+            listing = Listing(
+                title=title,
+                description=description, 
+                starting_bid=starting_bid,
+                image=image,
+                category=category,
+                owner=owner
+                )
+            # https://docs.djangoproject.com/en/4.0/ref/models/instances/#saving-objects
+            listing.save()
+            return redirect("listing", listing.id)
+        
+        else:
+            return render(request, "auctions/create_listing.html", {
+                "form": form
+            })
+
     return render(request, "auctions/create_listing.html", {
-        "NewListingForm" : NewListingForm()
+        "form" : NewListingForm()
     })
 
 def listing(request, listing_id):
