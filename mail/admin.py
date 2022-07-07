9e200cb9ea14caef97116bbbfe10602246b8f9ed
Smail <smailbestybay@gmail.com>
2022-07-07 01:02:42 -0700

    clear inner html before displaying email

--- mail/admin.py
+++ mail/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Email, User
 
+@admin.action(description='Mark selected emails read')
+def make_read(modeladmin, request, queryset):
+    queryset.update(read=True)
+
+@admin.action(description='Mark selected emails unread')
+def make_unread(modeladmin, request, queryset):
+    queryset.update(read=False)
+
+class MailAdmin(admin.ModelAdmin):
+    list_filter = ('read',)
+    list_display = ['user', 'sender','subject', 'read']
+    actions = [make_read, make_unread]
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Email)
+admin.site.register(Email, MailAdmin)
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -35,12 +35,13 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  document.querySelector('#email-view').style.display = 'none';
+  
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
- 
+  
 }
 
 function load_mailbox(mailbox) {
@@ -48,7 +49,9 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
+  
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
@@ -60,6 +63,63 @@ function load_mailbox(mailbox) {
       console.log(emails);
       
       // ... do something else with emails ...
+      
+      // for each email create div that displays sender, subject line, timestamp
+      // if read it should appear with grey background. 
+      emails.forEach(email => {
+        
+        const element = document.createElement('div');
+        if (email.read) {
+          element.setAttribute('class', 'email-item-read')
+        } else {
+          element.setAttribute('class', 'email-item')
+        }
+        element.innerHTML = `<span class="sender">${email.sender}</span> 
+        <span class="body">${email.body}</span> 
+        <span class="timestamp">${email.timestamp}</span>`;
+        element.addEventListener('click', function() {
+          get_email(email.id)
+        }); 
+        document.querySelector('#emails-view').append(element);
+      });
   });
+}
+
+function get_email(email_id) {
+  
+  document.querySelector('#email-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  const children = document.querySelectorAll('.email-info > span')
+  children.forEach(child => {
+    child.innerHTML = '';
+  })
+
+  document.querySelector('#email-body').innerHTML = '';
+
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Print email
+    console.log(email);
+    document.querySelector('#from').innerHTML += `<strong>From:</strong> ${email.sender}`;
+    document.querySelector('#to').innerHTML += `<strong>To:</strong> ${email.recipients}`;
+    document.querySelector('#subject').innerHTML += `<strong>Subject:</strong> ${email.subject}`;
+    document.querySelector('#timestamp').innerHTML += `<strong>Timestamp:</strong> ${email.timestamp}`;
+    document.querySelector('#email-body').innerHTML += ` ${email.body}`;
+    if (email.archived) {
+      document.querySelector('#archive-unarchive').innerHTML = 'Unarchive'
+    } else {
+      document.querySelector('#archive-unarchive').innerHTML = 'Archive'
+    };
+  });
+
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  })
 }
 
