b1dcf1999511f082fcd3354846761d36aa8be5a0
Smile <smailbestybay@gmail.com>
2022-07-25 03:00:01 -0700

    admin site and models mostly complete

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Post, UserFollowing
 
 # Register your models here.
+admin.site.register(User)
+# admin.site.register(Post)
+admin.site.register(UserFollowing)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'likes')
--- network/models.py
+++ network/models.py
@@ -3,4 +3,24 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    def __str__(self) -> str:
+        return f"{self.username}"
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"User: {self.user}, Short Content: {self.content[:20]}..."
+
+class UserFollowing(models.Model):
+    """ Don't forget to block from following the same user twice """
+
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following")
+    following_user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
+
+    def __str__(self) -> str:
+        return f"{self.user} is following {self.following_user}"
