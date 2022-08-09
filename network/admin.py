524077af8898c287d99d55c02e0f92182ea2b5a3
Smile <smailbestybay@gmail.com>
2022-08-08 22:37:13 -0700

    add custom method get_likes to post model

--- network/admin.py
+++ network/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import User, Post, UserFollowing
+from .models import User, Post, UserFollowing, Like
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserFollowing)
+admin.site.register(Like)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'likes', 'timestamp')
+    list_display = ('user', 'content', 'timestamp')
--- network/models.py
+++ network/models.py
@@ -13,12 +13,19 @@ class Post(models.Model):
     content = models.TextField(blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def get_likes(self):
+        """ Get count of likes """
+        return self.likes.count()
+
     def __str__(self) -> str:
         return f"User: {self.user}, Short Content: {self.content[:20]}..."
 
 class Like(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="likes")
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
 
 class UserFollowing(models.Model):
     """ Don't forget to block from following the same user twice """
